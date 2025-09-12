a6d443f1e809dc98be263303ddb49f5324bf935b
asif-shahriar11 <asif.asr11@gmail.com>
2025-09-12 22:13:57 +0000

    Deploying to gh-pages from @ asif-shahriar11/asif-shahriar11.github.io@8f8134f771899cb9d883a2299e542725cb3bc94f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "Publications",
           description: "Peer-reviewed publications in reversed chronological order. Greatly indebted to all my co-authors, supervisors, and collaborators for their invaluable contributions.",
           section: "Navigation",
