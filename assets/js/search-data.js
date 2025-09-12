12196519509f4aa2f9dc806882d975267db481d7
asif-shahriar11 <asif.asr11@gmail.com>
2025-09-12 22:17:53 +0000

    Deploying to gh-pages from @ asif-shahriar11/asif-shahriar11.github.io@01b34d8e6acd6670d60dea1366d13ee5b98a8f48 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-education",
-          title: "Education",
-          description: "A brief description of my academic journey (so far). Forever in debt of all of my teachers.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/education/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
@@ -50,6 +43,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-education",
+          title: "Education",
+          description: "A brief description of my academic journey (so far). Forever in debt of all of my teachers.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/education/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
