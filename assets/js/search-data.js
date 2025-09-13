e11bff1aa54b935efbfabaa8bc10dc5d741456de
asif-shahriar11 <asif.asr11@gmail.com>
2025-09-13 16:37:23 +0000

    Deploying to gh-pages from @ asif-shahriar11/asif-shahriar11.github.io@3568357f1f156f2a4981614b8323faab3daf0540 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-education",
           title: "Education",
           description: "A brief description of my academic journey (so far). Forever in debt of all of my teachers.",
