e691cc23c79cfaed60a3c19db9be946e1365a5cc
asif-shahriar11 <asif.asr11@gmail.com>
2025-09-13 18:18:47 +0000

    Deploying to gh-pages from @ asif-shahriar11/asif-shahriar11.github.io@9f16f038987aab0632f59696fd3720d163308ea4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Click the button on the right to download a PDF version. For more information, feel free to drop an email.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
