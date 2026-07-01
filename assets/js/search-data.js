fb951f084346e89ddf6eb5611385be6a53c0b8d6
asif-shahriar11 <asif.asr11@gmail.com>
2026-07-01 21:13:25 +0000

    Deploying to gh-pages from @ asif-shahriar11/asif-shahriar11.github.io@14e878e82379ad362fde760b12abfd847a680b36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,29 +44,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-landed-my-first-job-as-a-full-time-lecturer-in-cse-department-of-brac-university-hello-academia",
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "A selection of my GitHub activity and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "news-landed-my-first-job-as-a-full-time-lecturer-in-cse-department-of-brac-university-hello-academia",
           title: 'Landed my first job as a full-time lecturer in CSE department of BRAC...',
           description: "",
           section: "News",},{id: "news-my-first-authored-paper-xlnet-cnn-has-received-the-best-paper-award-in-nsyss-2024",
@@ -84,6 +69,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("mailto:%61%73%69%66.%61%73%72%31%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/asif-shahriar11", "_blank");
         },
       },{
         id: 'social-linkedin',
